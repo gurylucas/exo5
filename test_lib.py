920d928c7fedbd0c5e58a196ceee59ce33ad30ed
Lucas GURY <contact.gury@gmail.com>
2022-07-11 12:27:06 +0200

    oui

--- test_lib.py
+++ test_lib.py
@@ -1,8 +1,8 @@
 import lib
 
 def test_average():
-    input1 = [11, -11, 10, 20]
-    expected_result = 15
+    input1 = [10, 11, 12]
+    expected_result = 11
     actual_result = Average(input1)
     assert expected_result == actual_result , "C kc"
      
