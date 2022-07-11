be1eae00f68432b6087d42f5c4001b40f502cc75
Lucas GURY <contact.gury@gmail.com>
2022-07-11 12:25:32 +0200

    ajout fichier fonction

--- test_lib.py
+++ test_lib.py
@@ -2,7 +2,7 @@ import lib
 
 def test_average():
     input1 = [11, -11, 10, 20]
-    expected_result = 7.5
+    expected_result = 15
     actual_result = Average(input1)
     assert expected_result == actual_result , "C kc"
      
